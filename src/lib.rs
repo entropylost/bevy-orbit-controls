2a1a45e16d8b2a944eca22dd7721adbdcae5de15
iMplode nZ <rg@youxplode.com>
2021-04-10 16:55:38 -0700

    Updated for 0.5.

--- src/lib.rs
+++ src/lib.rs
@@ -28,12 +28,6 @@ use bevy::render::camera::Camera;
 
 const LINE_TO_PIXEL_RATIO: f32 = 0.1;
 
-#[derive(Default)]
-struct State {
-    motion: EventReader<MouseMotion>,
-    scroll: EventReader<MouseWheel>,
-}
-
 pub struct OrbitCamera {
     pub x: f32,
     pub y: f32,
@@ -49,7 +43,7 @@ impl Default for OrbitCamera {
             x: 0.0,
             y: 0.0,
             distance: 5.0,
-            center: Vec3::zero(),
+            center: Vec3::ZERO,
             rotate_sensitivity: 1.0,
             zoom_sensitivity: 0.8,
         }
@@ -73,13 +67,12 @@ pub struct OrbitCameraPlugin;
 impl OrbitCameraPlugin {
     fn mouse_motion_system(
         time: Res<Time>,
-        mut state: ResMut<State>,
-        mouse_motion_events: Res<Events<MouseMotion>>,
+        mut mouse_motion_events: EventReader<MouseMotion>,
         mouse_button_input: Res<Input<MouseButton>>,
         mut query: Query<(&mut OrbitCamera, &mut Transform, &mut Camera)>,
     ) {
-        let mut delta = Vec2::zero();
-        for event in state.motion.iter(&mouse_motion_events) {
+        let mut delta = Vec2::ZERO;
+        for event in mouse_motion_events.iter() {
             delta += event.delta;
         }
         for (mut camera, mut transform, _) in query.iter_mut() {
@@ -89,22 +82,21 @@ impl OrbitCameraPlugin {
 
                 camera.y = camera.y.max(0.01).min(3.13);
 
-                let rot = Quat::from_axis_angle(Vec3::unit_y(), camera.x)
-                    * Quat::from_axis_angle(-Vec3::unit_x(), camera.y);
+                let rot = Quat::from_axis_angle(Vec3::Y, camera.x)
+                    * Quat::from_axis_angle(-Vec3::X, camera.y);
                 transform.translation =
                     (rot * Vec3::new(0.0, 1.0, 0.0)) * camera.distance + camera.center;
-                transform.look_at(camera.center, Vec3::unit_y());
+                transform.look_at(camera.center, Vec3::Y);
             }
         }
     }
 
     fn zoom_system(
-        mut state: ResMut<State>,
-        mouse_wheel_events: Res<Events<MouseWheel>>,
+        mut mouse_wheel_events: EventReader<MouseWheel>,
         mut query: Query<(&mut OrbitCamera, &mut Transform, &mut Camera)>,
     ) {
         let mut total = 0.0;
-        for event in state.scroll.iter(&mouse_wheel_events) {
+        for event in mouse_wheel_events.iter() {
             total += event.y
                 * match event.unit {
                     Line => 1.0,
@@ -121,7 +113,7 @@ impl OrbitCameraPlugin {
 }
 impl Plugin for OrbitCameraPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.init_resource::<State>()
+        app
             .add_system(Self::mouse_motion_system.system())
             .add_system(Self::zoom_system.system());
     }
--- test/main.rs
+++ test/main.rs
@@ -11,26 +11,26 @@ fn main() {
 }
 
 fn startup(
-    commands: &mut Commands,
+    mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     commands
-        .spawn(PbrBundle {
+        .spawn_bundle(PbrBundle {
             mesh: meshes.add(Mesh::from(shape::Cube { size: 2.0 })),
             material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
             ..Default::default()
-        })
+        });
         // light
-        .spawn(LightBundle {
+    commands.spawn_bundle(LightBundle {
             transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
             ..Default::default()
-        })
+        });
         // camera
-        .spawn(Camera3dBundle {
+    commands.spawn_bundle(PerspectiveCameraBundle {
             transform: Transform::from_translation(Vec3::new(-3.0, 3.0, 5.0))
-                .looking_at(Vec3::default(), Vec3::unit_y()),
+                .looking_at(Vec3::default(), Vec3::Y),
             ..Default::default()
         })
-        .with(OrbitCamera::default());
+        .insert(OrbitCamera::default());
 }
