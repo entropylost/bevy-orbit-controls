4631627ea12582149f85a444c3e1a75c739cb82f
Moritz Moeller <virtualritz@gmail.com>
2021-04-12 20:11:15 +0200

    Updated example in docs, added version info to docs, added README, cargo fmt.

--- test/main.rs
+++ test/main.rs
@@ -1,7 +1,6 @@
 use bevy::prelude::*;
 use bevy_orbit_controls::*;
 
-#[bevy_main]
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
@@ -15,19 +14,19 @@ fn startup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    commands
-        .spawn_bundle(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Cube { size: 2.0 })),
-            material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-            ..Default::default()
-        });
-        // light
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 2.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        ..Default::default()
+    });
+    // light
     commands.spawn_bundle(LightBundle {
-            transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
-            ..Default::default()
-        });
-        // camera
-    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
+        ..Default::default()
+    });
+    // camera
+    commands
+        .spawn_bundle(PerspectiveCameraBundle {
             transform: Transform::from_translation(Vec3::new(-3.0, 3.0, 5.0))
                 .looking_at(Vec3::default(), Vec3::Y),
             ..Default::default()
